68915c3bef8d65742ee01d8a7668cec1b93aacec
AbbasMahdavi021 <abbas.mahdavi021@gmail.com>
2023-06-14 05:48:59 -0700

    Deploy to public Repo

--- README.md
+++ README.md
@@ -1,28 +1,17 @@
-# CSC 413 - Project One
+# CalculatorGUI: Mathematical Expression Evaluator
 
-## Student Name  : Abbas Mahdavi
+This assignment focuses on implementing an object-oriented design to create two programs: an evaluator for mathematical expressions and a graphical user interface (GUI) for the evaluator.
 
-## Student ID    : 918345420
+The provided Evaluator class (Evaluator.java) is almost complete, and the task is to program the utility classes it relies on: Operand and Operator. Follow the code suggestions to finalize the Evaluator class implementation. The Evaluator class offers a public method, eval, which takes a String parameter representing an infix mathematical expression. It parses and evaluates the expression, returning the integer result.
 
-## Student Email : amahdavi2@sfsu.edu  / (abbas.mahdavi021@gmail.com)
+Expressions consist of integer operands and operators such as +, -, *, /, ^, (, and ). Operators have predefined priorities, and the evaluator follows the rules of precedence when evaluating expressions.
 
-### Note failure to completely fill out this README.md could cause you to recieve a  0 for your project until correctly done
+Example expression: 2 + 3 * 4 evaluates to 14.
 
-# Adding the JUnit 5 Jars to your project.
+Requirements:
 
-### InteliJ
-In order to use the Unit tests given you will need to let intelij know where to find the needed jars. (jars are just java version of an archived folder or zip file.). 
-
-Intelij will actually find these jars for you while imporiting the project.
-
-If for some reason it does not, you can add the jars in the following way:
-
-1. Complete the process for "create project from existing resources"
-2. Then go to File -> Project Structure.
-3. Inside project Structure select the "Libraries" tab on the left.
-4. You will see a "+" symbol near the top of the window. CLick this symbol.
-5. A little window will popup asking what kind of library you want to import. Select "jar"
-6. After selection jar, a file selector will appear, navigate to the folder calcualtor/src/test/resources
-7. Add ALL jars in this folder. Then hit OK/Accept until all windows for project structure are closed.
-8. Now all jar needed to run unit test should be attached to your project.
-9. To run unit tests you can simply select any of the files in test/java. Also you may right-slick on the java folder inside test and select run all tests to run all given unit tests.
+Implement Operand and Operator classes.
+Complete the Evaluator class based on code suggestions.
+Use the eval method to evaluate infix mathematical expressions.
+Develop a GUI for the evaluator.
+Note: Detailed instructions and priority rules are provided in the code.
--- calculator/src/main/java/edu/csc413/calculator/evaluator/Evaluator.java
+++ calculator/src/main/java/edu/csc413/calculator/evaluator/Evaluator.java
@@ -11,16 +11,15 @@ public class Evaluator {
     private Stack<Operand> operandStack;
     private Stack<Operator> operatorStack;
     private StringTokenizer expressionTokenizer;
-    private final String delimiters = "() +/*-^";
+    private final String delimiters = " +/*-^()";
 
     public Evaluator() {
         operandStack = new Stack<>();
         operatorStack = new Stack<>();
     }
 
-    public int evaluateExpression(String expression) throws InvalidTokenException {
+    public int evaluateExpression(String expression ) throws InvalidTokenException {
         String expressionToken;
-
         // The 3rd argument is true to indicate that the delimiters should be used
         // as tokens, too. But, we'll need to remember to filter out spaces.
         this.expressionTokenizer = new StringTokenizer(expression, this.delimiters, true);
@@ -29,7 +28,6 @@ public class Evaluator {
         // the priority of any operator in the operator stack other than
         // the usual mathematical operators - "+-*/" - should be less than the priority
         // of the usual operators
-
 
         while (this.expressionTokenizer.hasMoreTokens()) {
             // filter out spaces
@@ -41,54 +39,49 @@ public class Evaluator {
                     if (!Operator.check(expressionToken)) {
                         throw new InvalidTokenException(expressionToken);
                     }
-
+                    // TODO Operator is abstract - these two lines will need to be fixed:
+                    // The Operator class should contain an instance of a HashMap,
+                    // and values will be instances of the Operators.  See Operator class
+                    // skeleton for an example.
                     Operator newOperator = Operator.getOperator(expressionToken);
-
-                    if ((operatorStack.isEmpty()) || (newOperator.priority() == 0)) {
-                        operatorStack.push(newOperator);
-                    } else {
-                        if ((newOperator.priority() == -1)) {
-                            while (operatorStack.peek().priority() != 0) {
-                                processExp ();
+                    while (!operatorStack.isEmpty() && operatorStack.peek().priority() >= newOperator.priority()) {
+                        if (operatorStack.peek().equals("(")) {
+                            operatorStack.push(new LeftParenthesesOperator());
+                        } else if (operatorStack.peek().equals(")")) {
+                            while (!operatorStack.peek().equals("(")) {
+                                processOperator();
                             }
-                            if (operatorStack.peek().priority() == 0) {
-                                operatorStack.pop();
-                            }
-                        } else {
-                            while ((operatorStack.peek().priority() >= newOperator.priority())) {
-                                processExp ();
-
-                                if (operatorStack.isEmpty()) {
-                                    break;
-                                }
-                            }
-                            operatorStack.push(newOperator);
+                            operatorStack.pop();
                         }
+                        operatorStack.pop();
+                    }
+                    if(!newOperator.equals(Operator.getOperator(")"))) {
+                        operatorStack.push(newOperator);
                     }
                 }
             }
         }
-        while (!operatorStack.isEmpty()) {
-            processExp ();
+
+        // Control gets here when we've picked up all of the tokens; you must add
+        // code to complete the evaluation - consider how the code given here
+        // will evaluate the expression 1+2*3
+        // When we have no more tokens to scan, the operand stack will contain 1 2
+        // and the operator stack will have + * with 2 and * on the top;
+        // In order to complete the evaluation we must empty the stacks,
+        // that is, we should keep evaluating the operator stack until it is empty;
+        // Suggestion: create a method that processes the operator stack until empty.
+        //operatorStack.clear();
+        while(!operatorStack.empty()) {
+            processOperator();
         }
-        return operandStack.pop().getOpValue();
+        return Integer.parseInt(Integer.toString(operandStack.pop().getValue()));
     }
 
-    private void processExp () {
+    void processOperator(){
         Operator operatorFromStack = operatorStack.pop();
         Operand operandTwo = operandStack.pop();
         Operand operandOne = operandStack.pop();
-        Operand result = operatorFromStack.execute(operandOne, operandTwo);
-        operandStack.push(result);
+        Operand result = operatorFromStack.execute( operandOne, operandTwo );
+        operandStack.push( result );
     }
 }
-
-
-    // Control gets here when we've picked up all of the tokens; you must add
-    // code to complete the evaluation - consider how the code given here
-    // will evaluate the expression 1+2*3
-    // When we have no more tokens to scan, the operand stack will contain 1 2
-    // and the operator stack will have + * with 2 and * on the top;
-    // In order to complete the evaluation we must empty the stacks,
-    // that is, we should keep evaluating the operator stack until it is empty;
-    // Suggestion: create a method that processes the operator stack until empty.
